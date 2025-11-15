e7bc777ef88b4922df282aba5dc740e0aadc2934
Angell2900 <Angelihimbazwe29@gmail.com>
2025-11-15 17:14:35 +0200

    done

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 
-@permission_required('bookshelf.can_view')
+@permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     # View logic here
     pass
 
-@permission_required('bookshelf.can_create')
+@permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
     # View logic here
     pass
 
-@permission_required('bookshelf.can_edit')
+@permission_required('bookshelf.can_edit', raise_exception=True)
 def book_edit(request, pk):
     # View logic here
     pass
 
-@permission_required('bookshelf.can_delete')
+@permission_required('bookshelf.can_delete', raise_exception=True)
 def book_delete(request, pk):
     # View logic here
     pass
