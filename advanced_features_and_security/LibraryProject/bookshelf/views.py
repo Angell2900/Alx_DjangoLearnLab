cabbb2a3c6e8bc089f8ffda5ecd4f278e42c1558
Angell2900 <Angelihimbazwe29@gmail.com>
2025-11-15 17:17:13 +0200

    forms.py changes

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm
+from .models import Book
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
-    # View logic here
-    pass
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
-    # View logic here
-    pass
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            Book.objects.create(
+                title=form.cleaned_data['title'],
+                author=form.cleaned_data['author'],
+                publication_year=form.cleaned_data['publication_year']
+            )
+            return render(request, 'bookshelf/form_example.html', {'form': form, 'message': 'Book created successfully'})
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def book_edit(request, pk):
