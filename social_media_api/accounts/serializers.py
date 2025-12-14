6789b122c8cdc837a523b8cbaaa073001ecce81d
Angell2900 <Angelihimbazwe29@gmail.com>
2025-12-14 22:31:27 +0300

    custom user model created

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,27 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from .models import CustomUser
 
-User = get_user_model()
-
+# Registration Serializer
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password']
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = CustomUser.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
         )
         return user
 
+# Login Serializer
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,36 +1,47 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
-from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from .models import CustomUser
 from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework.views import APIView
 
-User = get_user_model()
-
+# User Registration
 class RegisterAPIView(generics.CreateAPIView):
-    queryset = User.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
 
+# User Login
 class LoginAPIView(APIView):
-    serializer_class = LoginSerializer
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
-        return Response({"message": f"Welcome {user.username}!"})
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
 
+# User Profile
+class ProfileAPIView(generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = RegisterSerializer
+
+    def get_object(self):
+        return self.request.user
+
+# Follow / Unfollow Users
 class FollowUserAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        target_user = User.objects.get(id=user_id)
-        request.user.following.add(target_user)
-        return Response({"message": f"You are now following {target_user.username}"})
+        user_to_follow = CustomUser.objects.get(id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({'detail': f'You are now following {user_to_follow.username}'})
 
 class UnfollowUserAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        target_user = User.objects.get(id=user_id)
-        request.user.following.remove(target_user)
-        return Response({"message": f"You unfollowed {target_user.username}"})
+        user_to_unfollow = CustomUser.objects.get(id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'detail': f'You have unfollowed {user_to_unfollow.username}'})
