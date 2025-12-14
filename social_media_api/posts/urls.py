1123a3727d35eff465d085083a3a539abefbaba6
Angell2900 <Angelihimbazwe29@gmail.com>
2025-12-14 23:08:11 +0300

    CRUD feature for posts

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,8 +5,10 @@ from .views import PostViewSet, CommentViewSet, FeedViewSet
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
-router.register(r'feed', FeedViewSet, basename='feed')
+router.register(r'feed', FeedViewSet, basename='feed')  # <-- explicit feed endpoint
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('posts/<int:pk>/like/', PostViewSet.as_view({'post': 'like'}), name='post-like'),
+    path('posts/<int:pk>/unlike/', PostViewSet.as_view({'post': 'unlike'}), name='post-unlike'),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,21 +3,11 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Post, Comment, Like
+from .serializers import PostSerializer, CommentSerializer
 from accounts.models import CustomUser
 from notifications.models import Notification
-from .serializers import PostSerializer, CommentSerializer
 
-# Feed showing posts from users you follow
-class FeedViewSet(viewsets.ViewSet):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def list(self, request):
-        following_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-# Post CRUD + like/unlike
+# Post CRUD
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
@@ -25,6 +15,16 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if self.request.user != serializer.instance.author:
+            raise PermissionError("You cannot edit someone else's post")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if self.request.user != instance.author:
+            raise PermissionError("You cannot delete someone else's post")
+        instance.delete()
 
     @action(detail=True, methods=['POST'])
     def like(self, request, pk=None):
@@ -43,3 +43,32 @@ class PostViewSet(viewsets.ModelViewSet):
         post = get_object_or_404(Post, pk=pk)
         Like.objects.filter(user=request.user, post=post).delete()
         return Response({'status': 'post unliked'})
+
+# Comment CRUD
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if self.request.user != serializer.instance.author:
+            raise PermissionError("You cannot edit someone else's comment")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if self.request.user != instance.author:
+            raise PermissionError("You cannot delete someone else's comment")
+        instance.delete()
+
+# Feed view for posts from users you follow
+class FeedViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request):
+        following_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
